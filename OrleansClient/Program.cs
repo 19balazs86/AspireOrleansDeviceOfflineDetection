17907a32efcc8f18e84990ac82bcc0e7ed131001
Balazs Kereszturi <19balazs86@gmail.com>
2024-09-16 18:50:51 +0200

    Add initial project: OrleansClient

---
+++ OrleansClient/Program.cs
@@ -0,0 +1,24 @@
+namespace OrleansClient;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+        IServiceCollection services   = builder.Services;
+
+        // Add services to the container
+        {
+
+        }
+
+        WebApplication app = builder.Build();
+
+        // Configure the HTTP request pipeline
+        {
+            app.MapGet("/", () => "Hello from OrleansClient");
+        }
+
+        app.Run();
+    }
+}
