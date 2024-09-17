a45add42d9f4ec0c69a933c7c3681e833b0379cd
Balazs Kereszturi <19balazs86@gmail.com>
2024-09-17 19:06:19 +0200

    Add DeviceHub to DeviceGrain and NotifyStatusChanged

--- OrleansServer/Grains/DeviceGrain.cs
+++ OrleansServer/Grains/DeviceGrain.cs
@@ -1,5 +1,7 @@
-﻿using Orleans.GrainDirectory;
+﻿using Microsoft.AspNetCore.SignalR;
+using Orleans.GrainDirectory;
 using OrleansServer.GrainStates;
+using OrleansServer.Hubs;
 using Shared;
 
 namespace OrleansServer.Grains;
@@ -11,24 +13,30 @@ public sealed class DeviceGrain : Grain, IDeviceGrain
 
     private readonly IPersistentState<DeviceState> _state;
 
-    private readonly string _name;
+    private readonly IHubContext<DeviceHub, IDeviceHubClient> _deviceHub;
+
+    private readonly string _deviceId;
 
     public DeviceGrain(
         ILogger<DeviceGrain> logger,
-        [PersistentState(nameof(DeviceState))] IPersistentState<DeviceState> state)
+        [PersistentState(nameof(DeviceState))] IPersistentState<DeviceState> state,
+        IHubContext<DeviceHub, IDeviceHubClient> deviceHub)
     {
-        _logger = logger;
-        _state  = state;
-        _name   = this.GetPrimaryKeyString();
+        _logger    = logger;
+        _state     = state;
+        _deviceHub = deviceHub;
+        _deviceId  = this.GetPrimaryKeyString();
     }
 
     public async Task ReceiveHeartbeat()
     {
-        _logger.LogInformation("Device({name}) received a heartbeat", _name);
+        _logger.LogInformation("Device({name}) received a heartbeat", _deviceId);
 
         _state.State.LastHeartbeat = DateTime.UtcNow;
         _state.State.HeartbeatCount++;
 
         await _state.WriteStateAsync();
+
+        await _deviceHub.Clients.All.NotifyStatusChanged(_deviceId, "online"); // offline
     }
 }
