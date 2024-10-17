ef50c0e4b3dc925384534eb48a5eeea6fe3746ff
19balazs86 <19balazs86@gmail.com>
2024-10-17 12:26:24 +0200

    Removed unused using directives

--- IntegrationTest/ApplicationFixture.cs
+++ IntegrationTest/ApplicationFixture.cs
@@ -1,7 +1,4 @@
-﻿using Aspire.Hosting;
-using Aspire.Hosting.ApplicationModel;
-
-namespace IntegrationTest;
+﻿namespace IntegrationTest;
 
 public sealed class ApplicationFixture : IAsyncLifetime
 {
